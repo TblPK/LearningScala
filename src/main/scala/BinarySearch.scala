c68311ea5d69667fb37853b2321cc504edad3710
TblPK <sapaevmurat735@gmail.com>
2024-05-31 03:48:38 +0300

    fix BinarySearch and MergeSort

--- src/main/scala/BinarySearch.scala
+++ src/main/scala/BinarySearch.scala
@@ -1,23 +1,24 @@
 import scala.annotation.tailrec
 
-def binarySearchIterative(arr: Array[Int], tar: Int): Int =
-    var (left, right) = (0, arr.length - 1)
-    while (left <= right)
+object BinarySearch { // Time complexity: O(log(n)) Space complexity: O(1)
+
+    def binarySearchIterative(arr: Array[Int], tar: Int): Int =
+        var (left, right) = (0, arr.length - 1)
+        while (left <= right)
+            val mid = left + (right - left) / 2
+            mid match
+                case _ if arr(mid) == tar => return mid
+                case _ if arr(mid) < tar => left = mid + 1
+                case _ if arr(mid) > tar => right = mid - 1
+        -1 // return -1
+
+    @tailrec
+    def binarySearchRecursive(arr: Array[Int], tar: Int)(left: Int = 0, right: Int = arr.length - 1): Int =
         val mid = left + (right - left) / 2
-        arr(mid) match
-            case v if v == tar => return mid
-            case v if v < tar => left = mid + 1
-            case v if v > tar => right = mid - 1
-    -1 // return -1
+        mid match
+            case _ if left > right => -1
+            case _ if arr(mid) == tar => mid
+            case _ if arr(mid) < tar => binarySearchRecursive(arr, tar)(mid + 1, right)
+            case _ if arr(mid) > tar => binarySearchRecursive(arr, tar)(left, mid - 1)
 
-@tailrec
-def binarySearchRecursive(arr: Array[Int], tar: Int)(left: Int = 0, right: Int = arr.length - 1): Int =
-    val mid = left + (right - left) / 2
-    arr(mid) match
-        case v if left > right => -1
-        case v if v == tar => mid
-        case v if v < tar => binarySearchRecursive(arr, tar)(mid + 1, right)
-        case v if v > tar => binarySearchRecursive(arr, tar)(left, mid - 1)
-
-object BinarySearch // Time complexity: O(log(n)) Space complexity: O(1)
-
+}
--- src/main/scala/MergeSort.scala
+++ src/main/scala/MergeSort.scala
@@ -1,17 +1,17 @@
-import scala.annotation.tailrec
+object MergeSort { // Time complexity: O(n*log(n)) Space complexity: O(1)
 
-def mergeSort(list: List[Int]): List[Int] = list match
-    case Nil => Nil
-    case x :: Nil => List(x)
-    case _ =>
-        val (left, right) = list.splitAt(list.length / 2)
-        merge(mergeSort(left), mergeSort(right))
+    def mergeSort(list: List[Int]): List[Int] = list match
+        case Nil => Nil
+        case x :: Nil => List(x)
+        case _ =>
+            val (left, right) = list.splitAt(list.length / 2)
+            merge(mergeSort(left), mergeSort(right))
 
-private def merge(list1: List[Int], list2: List[Int]): List[Int] = (list1, list2) match
-    case (_, Nil) => list1
-    case (Nil, _) => list2
-    case (x :: xt, y :: yt) =>
-        if (x < y) x :: merge(xt, list2)
-        else y :: merge(yt, list1)
+    private def merge(list1: List[Int], list2: List[Int]): List[Int] = (list1, list2) match
+        case (_, Nil) => list1
+        case (Nil, _) => list2
+        case (x :: xt, y :: yt) =>
+            if (x < y) x :: merge(xt, list2)
+            else y :: merge(yt, list1)
 
-object MergeSort // Time complexity: O(n*log(n)) Space complexity: O(1)
+}
