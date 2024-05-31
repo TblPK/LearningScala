f7f97d0c541a21ddc80452e02ad2cf73e98fc87d
TblPK <sapaevmurat735@gmail.com>
2024-05-31 03:12:26 +0300

    add MergeSort and fix BinarySearch

--- src/main/scala/BinarySearch.scala
+++ src/main/scala/BinarySearch.scala
@@ -1,26 +1,23 @@
 import scala.annotation.tailrec
 
-object BinarySearch { // Time complexity: O(log(n)) Space complexity: O(1)
-
-  def binarySearchIterative(arr: Array[Int], tar: Int): Int = {
+def binarySearchIterative(arr: Array[Int], tar: Int): Int =
     var (left, right) = (0, arr.length - 1)
-    while (left <= right) {
-      val mid = left + (right - left) / 2
-      if (arr(mid) == tar) return mid
-      else if (arr(mid) < tar) left = mid + 1
-      else right = mid - 1
-    }
-    -1
-  }
+    while (left <= right)
+        val mid = left + (right - left) / 2
+        arr(mid) match
+            case v if v == tar => return mid
+            case v if v < tar => left = mid + 1
+            case v if v > tar => right = mid - 1
+    -1 // return -1
 
-  @tailrec
-  def binarySearchRecursive(arr: Array[Int], tar: Int)(left: Int = 0, right: Int = arr.length - 1): Int = {
+@tailrec
+def binarySearchRecursive(arr: Array[Int], tar: Int)(left: Int = 0, right: Int = arr.length - 1): Int =
     val mid = left + (right - left) / 2
-    arr match {
-      case _ if left > right => -1
-      case _ if arr(mid) == tar => mid
-      case _ if arr(mid) < tar => binarySearchRecursive(arr, tar)(mid + 1, right)
-      case _ if arr(mid) > tar => binarySearchRecursive(arr, tar)(left, mid - 1)
-    }
-  }
-}
+    arr(mid) match
+        case v if left > right => -1
+        case v if v == tar => mid
+        case v if v < tar => binarySearchRecursive(arr, tar)(mid + 1, right)
+        case v if v > tar => binarySearchRecursive(arr, tar)(left, mid - 1)
+
+object BinarySearch // Time complexity: O(log(n)) Space complexity: O(1)
+
---
+++ src/test/scala/MergeSortTest.scala
@@ -0,0 +1,64 @@
+import org.scalatest.funsuite.AnyFunSuiteLike
+
+class MergeSortTest extends AnyFunSuiteLike {
+
+    test("Empty list") {
+        val arr = List.empty[Int]
+        val res = List.empty[Int]
+        assert(MergeSort.mergeSort(arr) == res)
+    }
+
+    test("Single element list") {
+        val arr = List(1)
+        val res = List(1)
+        assert(MergeSort.mergeSort(arr) == res)
+    }
+
+    test("Two element sorted list") {
+        val arr = List(1, 2)
+        val res = List(1, 2)
+        assert(MergeSort.mergeSort(arr) == res)
+    }
+
+    test("Two element unsorted list") {
+        val arr = List(2, 1)
+        val res = List(1, 2)
+        assert(MergeSort.mergeSort(arr) == res)
+    }
+
+    test("Already sorted list") {
+        val arr = List(1, 2, 3, 4, 5)
+        val res = List(1, 2, 3, 4, 5)
+        assert(MergeSort.mergeSort(arr) == res)
+    }
+
+    test("Reverse sorted list") {
+        val arr = List(5, 4, 3, 2, 1)
+        val res = List(1, 2, 3, 4, 5)
+        assert(MergeSort.mergeSort(arr) == res)
+    }
+
+    test("Unsorted list with duplicates") {
+        val arr = List(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5)
+        val res = List(1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9)
+        assert(MergeSort.mergeSort(arr) == res)
+    }
+
+    test("Unsorted list") {
+        val arr = List(4, 2, 7, 1, 3, 6, 5)
+        val res = List(1, 2, 3, 4, 5, 6, 7)
+        assert(MergeSort.mergeSort(arr) == res)
+    }
+
+    test("List with negative numbers") {
+        val arr = List(-3, -1, -4, -1, -5, -9, -2, -6, -5, -3, -5)
+        val res = List(-9, -6, -5, -5, -5, -4, -3, -3, -2, -1, -1)
+        assert(MergeSort.mergeSort(arr) == res)
+    }
+
+    test("Mixed positive and negative numbers") {
+        val arr = List(3, -1, 4, -1, 5, -9, 2, -6, 5, -3, 5)
+        val res = List(-9, -6, -3, -1, -1, 2, 3, 4, 5, 5, 5)
+        assert(MergeSort.mergeSort(arr) == res)
+    }
+}
